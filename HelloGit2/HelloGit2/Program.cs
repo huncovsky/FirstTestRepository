eb840af31ce4e0546671e5cc9fa821bcc4085099
huncovsky <vaclav.huncovsky@accenture.com>
2016-05-19 19:34:10 +0200

    Add new line

--- HelloGit2/HelloGit2/Program.cs
+++ HelloGit2/HelloGit2/Program.cs
@@ -11,6 +11,7 @@ namespace HelloGit2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Worlds!");
+            Console.WriteLine("Footer!");
             Console.ReadKey();
         }
     }
