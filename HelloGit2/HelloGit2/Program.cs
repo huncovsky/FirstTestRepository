bd25074a0059e6472cc4f7f555f6ffb1bd679d60
huncovsky <vaclav.huncovsky@accenture.com>
2016-05-19 18:35:37 +0200

    Change to world

--- HelloGit2/HelloGit2/Program.cs
+++ HelloGit2/HelloGit2/Program.cs
@@ -10,7 +10,7 @@ namespace HelloGit2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello Worlds!");
             Console.ReadKey();
         }
     }
